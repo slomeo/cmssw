99d69bed7c1146e062ba6c1a3b96ee17a7f7ad28
Sergio Lo Meo <sergio.lomeo@cern.ch>
2019-09-04 10:50:14 +0200

    RPC Migration to DD4HEP

---
+++ Geometry/MuonNumbering/src/DD4hep_RPCNumberingScheme.cc
@@ -0,0 +1,241 @@
+// -*- C++ -*-
+//
+//
+/*
+
+ Description: RPC Numbering Scheme for DD4HEP 
+              based on DT Numbering Scheme made by Ianna Osburne 
+
+*/
+//
+//         Author:  Sergio Lo Meo (INFN Section of Bologna - Italy) sergio.lomeo@cern.ch
+//         Created:  Wed, 21 August 2019 16:00 CET
+//
+//
+#include "Geometry/MuonNumbering/interface/DD4hep_RPCNumberingScheme.h"
+#include "Geometry/MuonNumbering/interface/DD4hep_MuonNumbering.h"
+#include "DataFormats/MuonDetId/interface/RPCDetId.h"
+#include "Geometry/MuonNumbering/interface/MuonBaseNumber.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <iostream>
+#include <math.h>
+
+#include <cassert>
+
+using namespace cms;
+using namespace std;
+
+RPCNumberingScheme::RPCNumberingScheme(const MuonConstants& muonConstants) { initMe(muonConstants); }
+
+
+void RPCNumberingScheme::initMe(const MuonConstants& muonConstants) {
+
+  int levelPart = get("level", muonConstants);
+
+  assert(levelPart != 0);
+  theRegionLevel = get("mr_region", muonConstants) / levelPart;
+  theBWheelLevel = get("mr_bwheel", muonConstants) / levelPart;
+  theBStationLevel = get("mr_bstation", muonConstants) / levelPart;
+  theBPlaneLevel = get("mr_bplane", muonConstants) / levelPart;
+  theBChamberLevel = get("mr_bchamber", muonConstants) / levelPart;
+  theEPlaneLevel = get("mr_eplane", muonConstants) / levelPart;
+  theESectorLevel = get("mr_esector", muonConstants) / levelPart;
+  theERollLevel = get("mr_eroll", muonConstants) / levelPart;
+
+  cout<<"------------myDEBUG------------"<<endl;
+  cout<<"levelPart: "<<levelPart<<endl;
+  cout<<"RegionLevel: "<<theRegionLevel*levelPart<<endl;
+  cout<<"BWheelLevel: "<<theBWheelLevel*levelPart<<endl;
+  cout<<"BStationLevel: "<<theBStationLevel*levelPart<<endl;
+  cout<<"BPlaneLevel: "<<theBPlaneLevel*levelPart<<endl;
+  cout<<"BChamberLevel: "<<theBChamberLevel*levelPart<<endl;
+  cout<<"EPlaneLevel: "<<theEPlaneLevel*levelPart<<endl;
+  cout<<"ESectorLevel: "<<theESectorLevel*levelPart<<endl;
+  cout<<"ERollLevel: "<<theERollLevel*levelPart<<endl;
+  cout<<"-------------------------------"<<endl;
+
+  // RPCDetId(int region, int ring, int station, int sector, int layer, int subsector, int roll);
+}
+
+int RPCNumberingScheme::baseNumberToUnitNumber(const MuonBaseNumber& num) {
+
+ 
+  const int mylevel = num.getLevels();
+    cout<<"------------myDEBUG--------------------------------------------------"<<endl;
+    cout<<"DD4hep_RPCNumberingScheme.cc: levels "<<mylevel<<endl;
+    cout<<"DD4hep_RPCNumberingScheme.cc: superNo "<<num.getSuperNo(mylevel)<<endl;
+    cout<<"DD4hep_RPCNumberingScheme.cc: baseNo "<<num.getBaseNo(mylevel)<<endl;
+    cout<<"---------------------------------------------------------------------"<<endl;
+    const int barrel = num.getSuperNo(theRegionLevel);
+  bool barrel_muon = (barrel == 1);
+  int maxLevel;
+  if (barrel_muon) {
+    maxLevel = theBChamberLevel;
+  } else {
+    maxLevel = theERollLevel;
+  }
+
+  if (num.getLevels() != maxLevel) {
+
+    return 0;
+  }
+
+  int plane_id = 0;
+  int sector_id = 0;
+  int copy_id = 0;
+  int roll_id = 0;
+  int eta_id = 0;
+  int rr12_id = 0;
+  bool forward = false;
+
+  int sector_copy = 0;
+
+  //decode significant rpc levels
+
+  for (int level = 1; level <= maxLevel; level++) {
+    //decode
+    if (level == theRegionLevel) {
+      if (barrel_muon) {
+        roll_id = 0;
+      } else {
+        copy_id = 1;
+      }
+    }
+    if (barrel_muon) {
+      if (level == theBWheelLevel) {
+        const int copyno = num.getBaseNo(level);
+        eta_id = 4 + copyno;  //copyno= [0,4]
+      } else if (level == theBStationLevel) {
+        //-	const int station_tag = num.getSuperNo(level);
+        const int copyno = num.getBaseNo(level);
+
+        sector_id = copyno + 1;
+        if (sector_id == 13) {
+          sector_id = 4;
+          sector_copy = 1;
+        } else if (sector_id == 14) {
+          sector_id = 10;
+          sector_copy = 1;
+        }
+        // mltiply by 3 to merge with endcaps
+        sector_id *= 3;
+
+      } else if (level == theBPlaneLevel) {
+        const int plane_tag = num.getSuperNo(level);
+        //        const int copyno = num.getBaseNo(level);
+        if (plane_tag == 1) {
+          plane_id = 1;
+        } else if (plane_tag == 2) {
+          plane_id = 5;
+        } else if (plane_tag == 3) {
+          //          if(copyno == 1) {
+          //if(eta_id == 4 || eta_id == 8) {
+          //  plane_id=6;
+
+          plane_id = 2;
+          // }
+
+          //          std::cout<<" KONTROLA w RPCNumberingScheme: eta_id: "<<eta_id<<", plane_tag: "<<plane_tag<<", plane_id: "<<plane_id<<std::endl;
+        } else if (plane_tag == 4) {
+          //          if(copyno == 1) {
+          // if(eta_id == 4 || eta_id == 8) {
+          //  plane_id=2;
+          //} else {
+          plane_id = 6;
+          //}
+          //          std::cout<<" KONTROLA w RPCNumberingScheme: eta_id: "<<eta_id<<", plane_tag: "<<plane_tag<<", plane_id: "<<plane_id<<std::endl;
+        } else if (plane_tag == 5) {
+          plane_id = 3;
+        } else {
+          plane_id = 4;
+        }
+
+      } else if (level == theBChamberLevel) {
+        const int copyno = num.getBaseNo(level);
+        if ((plane_id == 4) && (sector_id == 4 * 3)) {
+          copy_id = sector_copy * 2 + copyno + 1;
+        } else if ((plane_id == 4) && (sector_id == 10 * 3)) {
+          copy_id = sector_copy + 1;
+        } else {
+          copy_id = copyno + 1;
+        }
+        const int rollno = num.getSuperNo(level);
+        roll_id = rollno;
+      }
+
+    } else {
+      if (level == theRegionLevel) {
+        const int copyno = num.getBaseNo(level);
+        forward = (copyno == 0);
+      } else if (level == theEPlaneLevel) {
+        const int plane_tag = num.getSuperNo(level);
+        const int rr12_tag = num.getBaseNo(level);
+        plane_id = plane_tag;
+        rr12_id = rr12_tag;
+      } else if (level == theESectorLevel) {
+        const int copyno = num.getBaseNo(level);
+        sector_id = copyno + 1;
+        if (rr12_id == 1) {
+          sector_id = sector_id * 2 - 1;
+        } else if (rr12_id == 2) {
+          sector_id = sector_id * 2;
+        }
+      } else if (level == theERollLevel) {
+        const int copyno = num.getBaseNo(level);
+        const int eta_tag = num.getSuperNo(level);
+
+        if ((eta_tag == 1) || (eta_tag == 4) || (eta_tag == 7) || (eta_tag == 8)) {
+          eta_id = 1;
+        } else if ((eta_tag == 2) || (eta_tag == 5)) {
+          eta_id = 2;
+        } else if ((eta_tag == 3) || (eta_tag == 6)) {
+          eta_id = 3;
+        }
+
+        if (forward)
+          eta_id = 12 - eta_id;
+
+        // increase sector id for 20 degree chambers
+
+        if ((eta_tag == 4) || (eta_tag == 7) || (eta_tag == 8)) {
+          sector_id *= 2;
+        }
+
+        roll_id = copyno + 1;
+      }
+    }
+  }
+
+  // collect all info
+
+  int trIndex = (eta_id * 10000 + plane_id * 1000 + sector_id * 10 + copy_id) * 10 + roll_id;
+  // fare delle print di questi parametri sia con DDD che con DD4HEP
+
+  // Build the actual numbering
+  RPCDetId id;
+  id.buildfromTrIndex(trIndex);
+  cout<<"From DD4Hep_RPCNumebringScheme.cc "<<endl;
+  //  cout<<"------------myDEBUG------------"<<endl;
+  // cout<<"trIndex: "<<trIndex<<endl;
+  //cout<<"-------------------------------"<<endl;
+
+  cout<<"------------myDEBUG------------"<<endl;
+  cout<<"id.rawId: "<<id.rawId()<<endl;
+  cout<<"-------------------------------"<<endl;
+
+  SetDetID(id.rawId());  
+  return id.rawId();
+
+}
+
+const int RPCNumberingScheme::get(const char* key, const MuonConstants& muonConstants) const {
+  int result(0);
+  auto const& it = (muonConstants.find(key));
+  if (it != end(muonConstants))
+    result = it->second;
+  return result;
+}
+
--- Geometry/RPCGeometryBuilder/src/RPCGeometryBuilderFromDDD.cc
+++ Geometry/RPCGeometryBuilder/src/RPCGeometryBuilderFromDDD.cc
@@ -1,7 +1,6 @@
-/** Implementation of the RPC Geometry Builder from DDD
+/*
  *
- *  \author Port of: MuDDDRPCBuilder (ORCA)
- *  \author M. Maggi - INFN Bari
+ *
  */
 #include "Geometry/RPCGeometryBuilder/src/RPCGeometryBuilderFromDDD.h"
 #include "Geometry/RPCGeometry/interface/RPCGeometry.h"
@@ -9,8 +8,11 @@
 
 #include <DetectorDescription/Core/interface/DDFilter.h>
 #include <DetectorDescription/Core/interface/DDFilteredView.h>
+#include <DetectorDescription/DDCMS/interface/DDFilteredView.h>
+#include <DetectorDescription/DDCMS/interface/DDCompactView.h>
 #include <DetectorDescription/Core/interface/DDSolid.h>
 
+#include "Geometry/MuonNumbering/interface/DD4hep_MuonNumbering.h"
 #include "Geometry/MuonNumbering/interface/MuonDDDNumbering.h"
 #include "Geometry/MuonNumbering/interface/MuonBaseNumber.h"
 #include "Geometry/MuonNumbering/interface/RPCNumberingScheme.h"
@@ -25,14 +27,26 @@
 
 #include <iostream>
 #include <algorithm>
+#include "DetectorDescription/DDCMS/interface/DDSpecParRegistry.h"
+//#include "DD4hep/Detector.h"
+#include "Geometry/MuonNumbering/interface/DD4hep_RPCNumberingScheme.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <iostream>
+#include <math.h>
+
+using namespace std;
 
 RPCGeometryBuilderFromDDD::RPCGeometryBuilderFromDDD(bool comp11) : theComp11Flag(comp11) {}
 
 RPCGeometryBuilderFromDDD::~RPCGeometryBuilderFromDDD() {}
 
+// for DDD
 RPCGeometry* RPCGeometryBuilderFromDDD::build(const DDCompactView* cview, const MuonDDDConstants& muonConstants) {
-  const std::string attribute = "ReadOutName";  // could come from .orcarc
-  const std::string value = "MuonRPCHits";      // could come from .orcarc
+  const std::string attribute = "ReadOutName";
+  const std::string value = "MuonRPCHits";
 
   // Asking only for the MuonRPC's
   DDSpecificsMatchesValueFilter filter{DDValue(attribute, value, 0.0)};
@@ -41,6 +55,19 @@ RPCGeometry* RPCGeometryBuilderFromDDD::build(const DDCompactView* cview, const 
   return this->buildGeometry(fview, muonConstants);
 }
 
+// for DD4hep
+RPCGeometry* RPCGeometryBuilderFromDDD::build(const cms::DDCompactView* cview, const cms::MuonNumbering& muonConstants) {
+  const std::string attribute = "ReadOutName";
+  const std::string value = "MuonRPCHits";
+  cms::DDFilteredView fview(cview->detector(), cview->detector()->worldVolume());
+  cms::DDSpecParRefs refs;
+  cms::DDSpecParRegistry mypar = cview->specpars(); 
+  mypar.filter(refs, attribute, value);
+  fview.mergedSpecifics(refs);
+  return this->buildGeometry(fview, muonConstants);
+}
+
+// for DDD
 RPCGeometry* RPCGeometryBuilderFromDDD::buildGeometry(DDFilteredView& fview, const MuonDDDConstants& muonConstants) {
   LogDebug("RPCGeometryBuilderFromDDD") << "Building the geometry service";
   RPCGeometry* geometry = new RPCGeometry();
@@ -250,3 +277,230 @@ RPCGeometry* RPCGeometryBuilderFromDDD::buildGeometry(DDFilteredView& fview, con
   }
   return geometry;
 }
+
+
+// for DD4hep
+RPCGeometry* RPCGeometryBuilderFromDDD::buildGeometry(cms::DDFilteredView& fview, const cms::MuonNumbering& muonConstants) {
+
+  // cout<<"------------myDEBUG------------"<<endl;
+  //  cout<<"inside buidlGeometry from DD4HEP"<<endl;
+  //  cout<<"-------------------------------"<<endl;
+
+
+  RPCGeometry* geometry = new RPCGeometry();
+
+  bool doSubDets = fview.firstChild();
+
+  //cout<<"------------myDEBUG------------"<<endl;
+  // cout<<"doSubDets: "<<doSubDets<<endl;
+  //cout<<"-------------------------------"<<endl;
+
+  while (doSubDets) {
+    cout<<"From RPCGeometryBuilderFromDDD.cc "<<endl;
+    //    cout<<"------------myDEBUG------------"<<endl;
+    // cout<<"inside while(doSubDets)"<<endl;
+    //cout<<"-------------------------------"<<endl;
+
+    /* // DDD
+    // Get the Base Muon Number (const MuonDDDConstants& muonConstants)
+    MuonDDDNumbering mdddnum(muonConstants);
+    MuonBaseNumber mbn = mdddnum.geoHistoryToBaseNumber(fview.geoHistory());
+    // Get the The Rpc det Id
+    RPCNumberingScheme rpcnum(muonConstants);
+    const int detid = rpcnum.baseNumberToUnitNumber(mbn);
+     */
+
+    MuonBaseNumber mbn = muonConstants.geoHistoryToBaseNumber(fview.history());
+    const int mylevel = mbn.getLevels();
+    cout<<"------------myDEBUG--------------------------------------------------"<<endl;
+    cout<<"RPCGeometryBuilderFromDDD.cc: levels "<<mylevel<<endl;
+    cout<<"RPCGeometryBuilderFromDDD.cc: superNo "<<mbn.getSuperNo(mylevel)<<endl;
+    cout<<"RPCGeometryBuilderFromDDD.cc: baseNo "<<mbn.getBaseNo(mylevel)<<endl;
+    cout<<"---------------------------------------------------------------------"<<endl;
+    
+    for (auto& i: muonConstants.values()) 
+      { 
+	cout<<"RPCGeometryBuilderFromDDD.cc  "<<i.first<<": "<<i.second<<endl; 
+      } 
+
+    cms::RPCNumberingScheme rpcnum(muonConstants.values());
+    
+    //cout<<"------------myDEBUG--------------------------------------------------"<<endl;
+    // cout<<"after line:  cms::RPCNumberingScheme rpcnum(muonConstants.values()); "<<endl;
+    //cout<<"---------------------------------------------------------------------"<<endl;
+
+    const int detid = rpcnum.baseNumberToUnitNumber(mbn);
+   
+    cout<<"------------myDEBUG------------"<<endl;
+    cout<<"detid: "<<detid<<endl;
+    cout<<"-------------------------------"<<endl;
+
+    RPCDetId rpcid(detid);   
+    RPCDetId chid(rpcid.region(), rpcid.ring(), rpcid.station(), rpcid.sector(), rpcid.layer(), rpcid.subsector(), 0);
+
+    // DD4HEP: to be implemented
+    // DDValue numbOfStrips("nStrips");
+    const int nStrips = 42;
+      
+    // std::vector<double> dpar = fview.logicalPart().solid().parameters(); //DDD
+    std::vector<double> dpar = fview.extractParameters();
+    //std::string name = fview.logicalPart().name().name();    //DDD
+    std::string_view name = fview.name();
+    
+    //DDTranslation tran = fview.translation();// DDD
+    const Double_t* tran = fview.trans();
+    //DDRotationMatrix rota = fview.rotation();// DDD
+    const Double_t* rota = fview.rot();  
+    // Surface::PositionType pos(tran.x() / cm, tran.y() / cm, tran.z() / cm);//DDD
+    Surface::PositionType pos(tran[0] / cm, tran[1] / cm, tran[2] / cm);
+   
+
+    Surface::RotationType rot(rota[0],
+			      rota[3],
+			      rota[6],
+			      rota[1],
+			      rota[4],
+			      rota[7],
+			      rota[2],
+			      rota[5],
+			      rota[8]);
+    
+    RPCRollSpecs* rollspecs = nullptr;
+    Bounds* bounds = nullptr;
+
+    if (dpar.size() == 3) {
+      const float width = dpar[0] / cm;
+      const float length = dpar[1] / cm;
+      const float thickness = dpar[2] / cm;
+      bounds = new RectangularPlaneBounds(width, length, thickness);
+      // const std::vector<float> pars = {width, length, float(numbOfStrips.doubles()[0])};//DDD
+      const std::vector<float> pars = {width, length, float(nStrips)};//to be implemented for DD4HEP     
+
+      if (!theComp11Flag) {
+	//if (tran.z() > -1500.) {//DDD
+	if (tran[2] > -1500.) {
+
+	  ROOT::Math::Rotation3D myMatrix = ROOT::Math::Rotation3D(-1.,0.,0.,0.,-1.,0.,0.,0.,1.);
+	  fview.rot(myMatrix);
+	}
+      }
+
+      //rollspecs = new RPCRollSpecs(GeomDetEnumerators::RPCBarrel, name, pars); //DDD
+      rollspecs = new RPCRollSpecs(GeomDetEnumerators::RPCBarrel, std::string(name).c_str(), pars);
+  
+    } else {
+      const float be = dpar[4] / cm;
+      const float te = dpar[8] / cm;
+      const float ap = dpar[0] / cm;
+      const float ti = 0.4 / cm;
+      //  TrapezoidalPlaneBounds*
+      bounds = new TrapezoidalPlaneBounds(be, te, ap, ti);
+      const std::vector<float> pars = {float(dpar[4] / cm) ,
+                                       float(dpar[8] / cm) ,
+                                       float(dpar[0] / cm) ,
+                                       float(nStrips)};
+      /* //DDD
+      const std::vector<float> pars = {float(dpar[4] / cm) ,
+                                       float(dpar[8] / cm) ,
+                                       float(dpar[0] / cm) ,
+                                       float(numbOfStrips.doubles()[0])};
+      */
+      // rollspecs = new RPCRollSpecs(GeomDetEnumerators::RPCEndcap, name, pars); //DDD
+      rollspecs = new RPCRollSpecs(GeomDetEnumerators::RPCEndcap, std::string(name).c_str(), pars);
+
+      ROOT::Math::Rotation3D myMatrix2 = ROOT::Math::Rotation3D(1.,0.,1.,0.,0.,-1.,0.,1.,0.);
+      fview.rot(myMatrix2);
+    }
+    
+    BoundPlane* bp = new BoundPlane(pos, rot, bounds);
+    ReferenceCountingPointer<BoundPlane> surf(bp);
+    RPCRoll* r = new RPCRoll(rpcid, surf, rollspecs);
+    geometry->add(r);
+
+    auto rls = chids.find(chid);
+    if (rls == chids.end())
+      rls = chids.insert(std::make_pair(chid, std::list<RPCRoll*>())).first;
+    rls->second.emplace_back(r);
+
+    doSubDets = fview.nextSibling(); 
+  }
+  
+  for (auto& ich : chids) {
+    const RPCDetId& chid = ich.first;
+    const auto& rls = ich.second;
+
+  
+    BoundPlane* bp = nullptr;
+    if (!rls.empty()) {
+  
+      const auto& refSurf = (*rls.begin())->surface();
+      if (chid.region() == 0) {
+        float corners[6] = {0, 0, 0, 0, 0, 0};
+        for (auto rl : rls) {
+          const double h2 = rl->surface().bounds().length() / 2;
+          const double w2 = rl->surface().bounds().width() / 2;
+          const auto x1y1AtRef = refSurf.toLocal(rl->toGlobal(LocalPoint(-w2, -h2, 0)));
+          const auto x2y2AtRef = refSurf.toLocal(rl->toGlobal(LocalPoint(+w2, +h2, 0)));
+          corners[0] = std::min(corners[0], x1y1AtRef.x());
+          corners[1] = std::min(corners[1], x1y1AtRef.y());
+          corners[2] = std::max(corners[2], x2y2AtRef.x());
+          corners[3] = std::max(corners[3], x2y2AtRef.y());
+
+          corners[4] = std::min(corners[4], x1y1AtRef.z());
+          corners[5] = std::max(corners[5], x1y1AtRef.z());
+        }
+        const LocalPoint lpOfCentre((corners[0] + corners[2]) / 2, (corners[1] + corners[3]) / 2, 0);
+        const auto gpOfCentre = refSurf.toGlobal(lpOfCentre);
+        auto bounds = new RectangularPlaneBounds(
+            (corners[2] - corners[0]) / 2, (corners[3] - corners[1]) / 2, (corners[5] - corners[4]) + 0.5);
+        bp = new BoundPlane(gpOfCentre, refSurf.rotation(), bounds);
+      } else {
+        float cornersLo[3] = {0, 0, 0}, cornersHi[3] = {0, 0, 0};
+        float cornersZ[2] = {0, 0};
+        for (auto rl : rls) {
+          const double h2 = rl->surface().bounds().length() / 2;
+          const double w2 = rl->surface().bounds().width() / 2;
+          const auto& topo = dynamic_cast<const TrapezoidalStripTopology&>(rl->specificTopology());
+          const double r = topo.radius();
+          const double wAtLo = w2 / r * (r - h2); 
+          const double wAtHi = w2 / r * (r + h2);
+
+          const auto x1y1AtRef = refSurf.toLocal(rl->toGlobal(LocalPoint(-wAtLo, -h2, 0)));
+          const auto x2y1AtRef = refSurf.toLocal(rl->toGlobal(LocalPoint(+wAtLo, -h2, 0)));
+          const auto x1y2AtRef = refSurf.toLocal(rl->toGlobal(LocalPoint(-wAtHi, +h2, 0)));
+          const auto x2y2AtRef = refSurf.toLocal(rl->toGlobal(LocalPoint(+wAtHi, +h2, 0)));
+
+          cornersLo[0] = std::min(cornersLo[0], x1y1AtRef.x());
+          cornersLo[1] = std::max(cornersLo[1], x2y1AtRef.x());
+          cornersLo[2] = std::min(cornersLo[2], x1y1AtRef.y());
+
+          cornersHi[0] = std::min(cornersHi[0], x1y2AtRef.x());
+          cornersHi[1] = std::max(cornersHi[1], x2y2AtRef.x());
+          cornersHi[2] = std::max(cornersHi[2], x1y2AtRef.y());
+
+          cornersZ[0] = std::min(cornersZ[0], x1y1AtRef.z());
+          cornersZ[1] = std::max(cornersZ[1], x1y1AtRef.z());
+        }
+        const LocalPoint lpOfCentre((cornersHi[0] + cornersHi[1]) / 2, (cornersLo[2] + cornersHi[2]) / 2, 0);
+        const auto gpOfCentre = refSurf.toGlobal(lpOfCentre);
+        auto bounds = new TrapezoidalPlaneBounds((cornersLo[1] - cornersLo[0]) / 2,
+                                                 (cornersHi[1] - cornersHi[0]) / 2,
+                                                 (cornersHi[2] - cornersLo[2]) / 2,
+                                                 (cornersZ[1] - cornersZ[0]) + 0.5);
+        bp = new BoundPlane(gpOfCentre, refSurf.rotation(), bounds);
+      }
+    }
+
+    ReferenceCountingPointer<BoundPlane> surf(bp);
+   
+    RPCChamber* ch = new RPCChamber(chid, surf);
+   
+    for (auto rl : rls)
+      ch->add(rl);
+   
+    geometry->add(ch);
+  }
+
+   return geometry;
+}
+
