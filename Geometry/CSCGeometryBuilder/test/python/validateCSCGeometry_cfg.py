07055bb188bc80ecc6b513008e18c385a635f388
Sergio Lo Meo <sergio.lomeo@cern.ch>
2019-10-21 19:47:51 +0200

    start Validate Class

---
+++ Geometry/CSCGeometryBuilder/test/python/validateCSCGeometry_cfg.py
@@ -0,0 +1,50 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process('VALID')
+
+process.source = cms.Source('EmptySource')
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(1)
+    )
+
+process.load('Configuration.StandardSequences.DD4hep_GeometrySim_cff')
+process.load("FWCore.MessageLogger.MessageLogger_cfi")
+process.load("Geometry.MuonNumbering.muonNumberingInitialization_cfi")
+
+
+process.CSCGeometryESProducer = cms.ESProducer("CSCGeometryESModule",
+                                               DDDetector = cms.ESInputTag('',''),
+                                               applyAlignment = cms.bool(False),
+                                               alignmentsLabel = cms.string(''),
+                                               appendToDataLabel = cms.string(''),
+                                               attribute = cms.string('MuStructure'),
+                                               value = cms.string('MuonEndcapCSC'),
+                                               useDDD = cms.untracked.bool(False),
+                                               useDD4hep = cms.untracked.bool(True)
+                                              )
+
+process.DDSpecParRegistryESProducer = cms.ESProducer("DDSpecParRegistryESProducer",
+                                                     appendToDataLabel = cms.string('') # it was ' ' in RPC
+                                                     )
+
+process.MuonNumberingESProducer = cms.ESProducer("MuonNumberingESProducer",
+                                                 label = cms.string(''),# it was ' ' in RPC
+                                                 key = cms.string('MuonCommonNumbering')
+                                                 )
+
+process.test = cms.EDAnalyzer("DDTestMuonNumbering")
+
+#
+# Note: Please, download the geometry file from a location
+#       specified by Fireworks/Geometry/data/download.url
+#
+# For example: wget http://cmsdoc.cern.ch/cms/data/CMSSW/Fireworks/Geometry/data/v4/cmsGeom10.root
+#
+process.valid = cms.EDAnalyzer("CSCGeometryValidate",
+                               infileName = cms.untracked.string('cmsRecoGeom-2021.root'),
+                               outfileName = cms.untracked.string('validateCSCGeometry.root'),
+                               tolerance = cms.untracked.int32(7)
+                               )
+
+process.p = cms.Path(process.valid)# process.test per 
